44b45c61d65ced5ad7d15920e0f65ee25fc9c35a
MarkChappell <MarkChappell@users.noreply.github.com>
2014-10-26 10:09:42 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix
+##  returns a list for performing the following functions
+##   - setting the matrix (set)
+##   - getting the matrix (get)
+##   - setting the inverse of the matrix (setinverse)
+##   - getting the inverse of the matrix (getinverse)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## set the value of the matrix
+  inv<- NULL                             # initialize the inverse of the matrix
+  set <- function(y) {
+    x <<- y                              # the <<- is a super assignment operator
+    inv <<- NULL                         # to update the variable in the parent environment
+  }
+  ## get the value of the matrix
+  get <- function() x
+  
+  ## set the inverse of the matrix
+  setinverse <- function(solve) inv <<- solve  
+  
+  ## get the inverse of the matrix
+  getinverse <- function() inv
+  
+  ## return the requested list
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve
+##  returns a matrix depending on the following:
+##  - if the cache exists, return its contents
+##  - if the cache does not exist, compute the inverse of the matrix
+##    and store the results in the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## retrieve the inverse for the matrix        
+  inv <- x$getinverse()
+  
+  ## check the contents of the matrix 
+  if(!is.null(inv)) {                        # the matrix found, return it from the cache
+    message("getting cached data")           # inform the user that the data is from the cache
+    return(inv)
+  }
+  ## if the matrix is empty, return the inverse   
+  data <- x$get()
+  inv <- solve(data, ...)                    # solve is a generic function that solves an equation
+  x$setinverse(inv)
+  inv
 }
+
+
